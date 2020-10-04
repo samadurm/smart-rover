c767469bc6625bfd11d832a4e6a53e5760d8ab04
samadurm <samadurm@oregonstate.edu>
2020-10-03 22:31:03 -0700

    Added script to control camera angle from the mouse:

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+    public float mouseSensitivity = 50f;
+    public Transform playerBody;
+
+    private float xRot = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRot, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
