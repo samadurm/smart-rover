7a1bdffc4565d1a595399276d3d5939c4537d83b
samadurm <samadurm@oregonstate.edu>
2020-10-03 17:24:42 -0700

    added basic movement. this laucnhes rather than smoothly accelerates however

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -6,14 +6,61 @@ public class MovementScript : MonoBehaviour
 {
     public Rigidbody rb; 
 
+    public float forwardForce = 20.0f;
+    public float horizontalForce = 20.0f;
+
+    public bool moveUp = false;
+    public bool moveDown = false;
+    public bool moveRight = false;
+    public bool moveLeft = false;
+
     // Start is called before the first frame update
     void Start()
     {
     }
 
     // Update is called once per frame
+    void Update(){
+        if (Input.GetKey("up"))
+            moveUp = true;
+        if (Input.GetKey("down"))
+            moveDown = true;
+        if (Input.GetKey("left"))
+            moveLeft = true;
+        if (Input.GetKey("right"))
+            moveRight = true;
+    }
+
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        if(moveUp)
+            upMovement();
+        if(moveDown)
+            downMovement();
+        if (moveRight) 
+            rightMovement();
+        if(moveLeft)
+            leftMovement();
+        
+    }
+
+    void upMovement()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);    
+    }
+
+    void downMovement()
+    {
+        rb.AddForce(0, 0, -0.5f * forwardForce * Time.deltaTime);
+    }
+
+    void rightMovement()
+    {
+        rb.AddForce(horizontalForce * Time.deltaTime, 0, 0);
+    }
+
+    void leftMovement()
+    {
+        rb.AddForce(-horizontalForce * Time.deltaTime, 0, 0);
     }
 }
