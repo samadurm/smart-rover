e596c753898f06c4309c46f4d8bc89238d88be8c
samadurm <samadurm@oregonstate.edu>
2020-10-03 22:07:56 -0700

    Moved scripts into a subfolder

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MovementScript : MonoBehaviour
+{
+    public float speed = 6;
+    public CharacterController controller; 
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
