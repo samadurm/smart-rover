389855f4b4a114b302de7906ed8348c487339621
samadurm <samadurm@oregonstate.edu>
2020-10-03 23:10:19 -0700

    Added camera to the rover object. Had to reverse x and z axis since the 'front' of the rover was actually the side. will need to change back once that is fixed

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -24,6 +24,7 @@ public class MouseLook : MonoBehaviour
         xRot = Mathf.Clamp(xRot, -90f, 90f);
 
         transform.localRotation = Quaternion.Euler(xRot, 0, 0);
+
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
